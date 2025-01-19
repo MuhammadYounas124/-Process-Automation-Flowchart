e1372459188a13c9ad4f3c0b2bed705ef7f30664
MuhammadYounas124 <younasraja300@gmail.com>
2025-01-19 17:01:28 +0300

    add toolbox item like start , process, decision, end

---
+++ my-flowchart-app/src/toolbox.tsx
@@ -0,0 +1,15 @@
+const toolbox = () => {
+
+    return (
+        <div style={{ padding: '10px', border: '1px solid #ddd', width: '200px' }}>
+            <h4>ToolBox</h4>
+            <div className="tool-item">Start Node</div>
+            <div className="tool-item">Process Node</div>
+            <div className="tool-item">Decision Node</div>
+            <div className="tool-item">End Node</div>
+        </div>
+    );
+
+};
+
+export default toolbox;
