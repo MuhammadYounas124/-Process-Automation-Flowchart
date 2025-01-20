fea95b9a23495abb8e72cb63b9a3be1bdc7a7f1d
MuhammadYounas124 <younasraja300@gmail.com>
2025-01-20 12:08:27 +0300

    add import of different component in app and then app import in main

--- my-flowchart-app/src/App.tsx
+++ my-flowchart-app/src/App.tsx
@@ -1,1 +1,20 @@
+import FlowCanvas from "./Flowcanvas";
+import Propertypanel from "./Propertypanel";
+import { useState } from "react";
+import Toolbox from "./toolbox";
 
+const App = () => {
+
+    const [selectedNode, _setSelectedNode] = useState(null);
+    return(
+
+        <div style={{ display: 'flex', height: '100vh' }}>
+        <Toolbox />
+        <FlowCanvas />
+        <Propertypanel selectednode={selectedNode} />
+      </div>
+
+    );
+};
+
+export default App;
